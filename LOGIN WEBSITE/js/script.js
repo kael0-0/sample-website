4081efff98e1906488af02c388e079f8a085a5f9
kael0-0 <michaelbete29@gmail.com>
2024-06-27 21:33:02 +0800

    Add files via upload

---
+++ LOGIN WEBSITE/js/script.js
@@ -0,0 +1,28 @@
+//The navbar function and the login singup form funtion
+const navbarMenu = document.querySelector(".navbar .links");
+const menuBtn = document.querySelector(".menu_btn"); 
+const hideMenuBtn = navbarMenu.querySelector(".close_btn");
+const showPopupBtn = document.querySelector(".login_btn");
+const hidePopupBtn = document.querySelector(".form_popup .close_btn");
+const formPopup = document.querySelector(".form_popup");
+const loginSignupLink = document.querySelectorAll(".form_box .bottom_link a");
+
+menuBtn.addEventListener("click", () =>{
+  navbarMenu.classList.toggle("show-menu");
+});
+
+hideMenuBtn.addEventListener("click",() =>menuBtn.click());
+
+//show the popup form
+showPopupBtn.addEventListener("click", () =>{
+  document.body.classList.toggle("show-popup");
+});
+//hide the popup form
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+  link.addEventListener("click", (e) =>{
+    e.preventDefault();
+    formPopup.classList[link.id ===  "signup_link" ? 'add' : 'remove']("show-signup")
+  });
+});
